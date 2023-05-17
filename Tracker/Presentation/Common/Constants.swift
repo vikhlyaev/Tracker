aa3fa2bda4e0bd166dd19f06fd36055148ac0f03
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-17 23:27:12 +0500

    Adds constants

---
+++ Tracker/Presentation/Common/Constants.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+struct Constants {
+    struct Images {
+        static let trackersEmptyTrackerList = "icon-empty-tracker-list"
+    
+        static let statisticEmptyStatistic = "icon-empty-statistic"
+    }
+    
+    struct Text {
+        static let trackersTitle = "Трекеры"
+        static let trackersEmptyTrackerList = "Что будем отслеживать?"
+        
+        static let statisticTitle = "Статистика"
+        static let statisticEmptyStatistic = "Анализировать пока нечего"
+    }
+}
