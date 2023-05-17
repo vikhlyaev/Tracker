8fff3065788be9897b156fb363b06bc8f0b323d1
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-17 23:27:25 +0500

    Updates TrackersViewController

--- Tracker/Presentation/Trackers/TrackersViewController.swift
+++ Tracker/Presentation/Trackers/TrackersViewController.swift
@@ -2,6 +2,23 @@ import UIKit
 
 final class TrackersViewController: UIViewController {
     
+    private lazy var emptyTrackerListImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: Constants.Images.trackersEmptyTrackerList)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var emptyTrackerListLabel: UILabel = {
+        let label = UILabel()
+        label.text = Constants.Text.trackersEmptyTrackerList
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .appBlack
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -9,14 +26,21 @@ final class TrackersViewController: UIViewController {
         
         setupView()
         setConstraints()
+        setupNavigationBar()
     }
     
     // MARK: - Setup UI
     
     private func setupView() {
         view.backgroundColor = .systemBackground
+        view.addSubview(emptyTrackerListImageView)
+        view.addSubview(emptyTrackerListLabel)
     }
     
+    private func setupNavigationBar() {
+        title = Constants.Text.trackersTitle
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
 }
 
 // MARK: - Setting Constraints
@@ -25,7 +49,15 @@ extension TrackersViewController {
     
     private func setConstraints() {
         NSLayoutConstraint.activate([
+            emptyTrackerListImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptyTrackerListImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            emptyTrackerListImageView.widthAnchor.constraint(equalToConstant: 80),
+            emptyTrackerListImageView.heightAnchor.constraint(equalToConstant: 80),
             
+            emptyTrackerListLabel.topAnchor.constraint(equalTo: emptyTrackerListImageView.bottomAnchor, constant: 8),
+            emptyTrackerListLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            emptyTrackerListLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            emptyTrackerListLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     
