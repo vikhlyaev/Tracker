f44a855a0a986e1da4c80a8da6c98519b81dce6b
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-17 22:39:44 +0500

    Updates StatisticViewController

--- Tracker/Presentation/Statistic/StatisticViewController.swift
+++ Tracker/Presentation/Statistic/StatisticViewController.swift
@@ -2,6 +2,23 @@ import UIKit
 
 final class StatisticViewController: UIViewController {
     
+    private lazy var emptyStatisticImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: Constants.Images.statisticEmptyStatistic)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var emptyStatisticLabel: UILabel = {
+        let label = UILabel()
+        label.text = Constants.Text.statisticEmptyStatistic
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .appBlack
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -9,14 +26,21 @@ final class StatisticViewController: UIViewController {
         
         setupView()
         setConstraints()
+        setupNavigationBar()
     }
     
     // MARK: - Setup UI
     
     private func setupView() {
         view.backgroundColor = .systemBackground
+        view.addSubview(emptyStatisticImageView)
+        view.addSubview(emptyStatisticLabel)
     }
     
+    private func setupNavigationBar() {
+        title = Constants.Text.statisticTitle
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
 }
 
 // MARK: - Setting Constraints
@@ -25,7 +49,15 @@ extension StatisticViewController {
     
     private func setConstraints() {
         NSLayoutConstraint.activate([
+            emptyStatisticImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptyStatisticImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            emptyStatisticImageView.widthAnchor.constraint(equalToConstant: 80),
+            emptyStatisticImageView.heightAnchor.constraint(equalToConstant: 80),
             
+            emptyStatisticLabel.topAnchor.constraint(equalTo: emptyStatisticImageView.bottomAnchor, constant: 8),
+            emptyStatisticLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            emptyStatisticLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            emptyStatisticLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     
