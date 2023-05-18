af1da7d1374b96eeb2ea2bee0733ff0aba790016
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-18 19:50:21 +0500

    Adds NewIrregularEventViewController

---
+++ Tracker/Presentation/NewIrregularEvent/NewIrregularEventViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+final class NewIrregularEventViewController: UIViewController {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = Constants.Text.newIrregularEventTitle
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textColor = .appBlack
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+        setConstraints()
+    }
+    
+    // MARK: - Setup UI
+    
+    private func setupView() {
+        view.backgroundColor = .appWhite
+        view.addSubview(titleLabel)
+    }
+}
+
+// MARK: - Setting Constraints
+
+extension NewIrregularEventViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 27),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+    }
+}
+
