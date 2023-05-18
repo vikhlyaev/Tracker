34db80c1c366e80da000e080f610e531efa48b4e
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-18 16:12:20 +0500

    Adds UIStackView extensions

---
+++ Tracker/Extensions/UIKit/UIStackView + Extensions.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
