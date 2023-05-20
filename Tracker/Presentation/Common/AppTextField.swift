a2c9c412eea656c1b41e1b0c44eced4edddaca63
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-20 10:57:20 +0500

    Adds common view AppTextField

---
+++ Tracker/Presentation/Common/AppTextField.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+final class AppTextField: UITextField {
+    
+    init(with placeholder: String) {
+        super.init(frame: .zero)
+        setupView(with: placeholder)
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView(with placeholder: String) {
+        leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: frame.height))
+        leftViewMode = .always
+        clearButtonMode = .always
+        layer.cornerRadius = 10
+        backgroundColor = .appBackground.withAlphaComponent(0.3)
+        self.placeholder = placeholder
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+// MARK: - Setting Constraints
+
+extension AppTextField {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 75)
+        ])
+    }
+}
