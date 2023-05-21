083b54c631ead495485463eee8c750fadf04c755
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-05-21 14:51:22 +0500

    Adds button states and styles

--- Tracker/Presentation/Common/AppButton.swift
+++ Tracker/Presentation/Common/AppButton.swift
@@ -2,11 +2,44 @@ import UIKit
 
 final class AppButton: UIButton {
     
+    // MARK: - Enums
+    
+    enum ButtonState {
+        case enabled
+        case disabled
+    }
+    
+    enum ButtonStyle {
+        case normal
+        case cancel
+    }
+    
+    // MARK: - Properties
+
+    private var disabledBackgroundColor = UIColor.appGray
+    private var enabledBackgroundColor = UIColor.appBlack {
+        didSet {
+            backgroundColor = enabledBackgroundColor
+        }
+    }
+
+    private var style: ButtonStyle = .normal
+    private var height: CGFloat = 60
     private var action: (() -> Void)?
     
-    convenience init(with title: String, and action: @escaping () -> Void) {
+    // MARK: - Init
+    
+    convenience init(
+        title: String,
+        style: ButtonStyle = .normal,
+        height: CGFloat = 60,
+        action: @escaping () -> Void
+    ) {
         self.init(type: .system)
+        self.style = style
+        self.height = height
         self.action = action
+        
         setTitle(title, for: .normal)
         setupButton()
         setConstraints()
@@ -20,19 +53,40 @@ final class AppButton: UIButton {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Setup UI
+    
+    override var isEnabled: Bool {
+        didSet {
+            switch style {
+            case .normal:
+                backgroundColor = isEnabled ? enabledBackgroundColor : disabledBackgroundColor
+            case .cancel:
+                layer.borderColor = isEnabled ? UIColor.appRed.cgColor : UIColor.appGray.cgColor
+            }
+        }
+    }
+    
     private func setupButton() {
-        backgroundColor = .appBlack
+        switch style {
+        case .normal:
+            backgroundColor = enabledBackgroundColor
+            setTitleColor(.appWhite, for: .normal)
+            setTitleColor(.appWhite, for: .disabled)
+        case .cancel:
+            backgroundColor = .clear
+            layer.borderColor = UIColor.appRed.cgColor
+            layer.borderWidth = 1
+            setTitleColor(.appRed, for: .normal)
+            setTitleColor(.appGray, for: .disabled)
+        }
+        
         layer.cornerRadius = 16
-        
-        tintColor = .appWhite
-        
         titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
-        titleLabel?.textColor = .appWhite
-        
         addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        
         translatesAutoresizingMaskIntoConstraints = false
     }
+    
+    // MARK: - Action
     
     @objc
     private func buttonTapped() {
