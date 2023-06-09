6769a6d9fbef695569c5bc2ef98aea6cf5db5962
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-06-09 19:45:44 +0500

    Changes appBackground color

--- Tracker/Extensions/UIKit/UIColor + Extensions.swift
+++ Tracker/Extensions/UIKit/UIColor + Extensions.swift
@@ -19,8 +19,8 @@ extension UIColor {
     static var appBackground: UIColor {
         UIColor { (traits) -> UIColor in
             traits.userInterfaceStyle == .light ?
-            UIColor(red: 0.90, green: 0.91, blue: 0.92, alpha: 0.30) :
-            UIColor(red: 0.10, green: 0.10, blue: 0.10, alpha: 0.85)
+            UIColor(red: 0.90, green: 0.91, blue: 0.92, alpha: 0.70) :
+            UIColor(red: 0.25, green: 0.25, blue: 0.25, alpha: 0.85)
         }
     }
     static let appGray = UIColor(red: 0.68, green: 0.69, blue: 0.71, alpha: 1.00)
