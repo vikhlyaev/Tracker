e9e9571de9322a2a4e201dab88eb9b66c3355bbc
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-06-13 14:08:33 +0500

    Adds Tracker model

---
+++ Tracker/Models/Tracker.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+struct Tracker {
+    let id: UUID = UUID()
+    let name: String
+    let color: UIColor
+    let emoji: String
+    let schedule: [WeekDay]
+}
+
+typealias Trackers = [Tracker]
