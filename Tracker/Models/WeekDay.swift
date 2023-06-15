cdd8ab86be21a3772ac93a66796a2b13b28aeea2
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-06-15 09:42:26 +0500

    Adds number value for weekday

--- Tracker/Models/WeekDay.swift
+++ Tracker/Models/WeekDay.swift
@@ -1,11 +1,30 @@
 import Foundation
 
-enum WeekDay: String, CaseIterable {
-    case monday = "Понедельник"
-    case tuesday = "Вторник"
-    case wednesday = "Среда"
-    case thursday = "Четверг"
-    case friday = "Пятница"
-    case saturday = "Суббота"
-    case sunday = "Воскресенье"
+enum WeekDay: Int, CaseIterable {
+    case monday = 2
+    case tuesday = 3
+    case wednesday = 4
+    case thursday = 5
+    case friday = 6
+    case saturday = 7
+    case sunday = 1
+    
+    var title: String {
+        switch self {
+        case .monday:
+            return "Понедельник"
+        case .tuesday:
+            return "Вторник"
+        case .wednesday:
+            return "Среда"
+        case .thursday:
+            return "Четверг"
+        case .friday:
+            return "Пятница"
+        case .saturday:
+            return "Суббота"
+        case .sunday:
+            return "Воскресенье"
+        }
+    }
 }
