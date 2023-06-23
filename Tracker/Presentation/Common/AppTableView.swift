57fcb8e6041d8f5c8a3d7b01203abd13d63fb6e7
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-06-23 14:56:36 +0500

    Adds AppTableView

---
+++ Tracker/Presentation/Common/AppTableView.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+final class AppTableView: UITableView {
+    
+    override var contentSize:CGSize {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        return CGSize(width: UIView.noIntrinsicMetric, height: contentSize.height)
+    }
+    
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        layer.cornerRadius = 10
+    }
+    
+}
