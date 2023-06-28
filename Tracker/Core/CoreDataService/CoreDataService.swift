b2b652bbc15105327ee646f78c8533b3f533a3f7
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-06-28 22:48:09 +0500

    Removes CoreDataService

--- Tracker/Core/CoreDataService/CoreDataService.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import CoreData
-
-protocol CoreDataService {
-    func createTrackerFetchedResultsController() -> NSFetchedResultsController<TrackerManagedObject>
-    func createTrackerCategoryFetchedResultsController() -> NSFetchedResultsController<TrackerCategoryManagedObject>
-    func createTrackerRecordFetchedResultsController() -> NSFetchedResultsController<TrackerRecordManagedObject>
-}
