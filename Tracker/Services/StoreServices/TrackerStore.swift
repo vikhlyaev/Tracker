120af215ad61a4b2b87ec4ebc61dbe2e1aa5ca03
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-07-02 22:33:44 +0500

    Updates TrackerStore

--- Tracker/Services/StoreServices/TrackerStore.swift
+++ Tracker/Services/StoreServices/TrackerStore.swift
@@ -2,18 +2,55 @@ import Foundation
 import CoreData
 
 final class TrackerStore: NSObject {
-    
+    private var insertedIndexes: IndexSet?
+    private var deletedIndexes: IndexSet?
     private let dataStore: DataStore
     private let context: NSManagedObjectContext
+    private weak var delegate: StoreDelegate?
     
-    init(dataStore: DataStore) {
+    private lazy var fetchedResultController: NSFetchedResultsController<TrackerManagedObject> = {
+        let request = TrackerManagedObject.fetchRequest()
+        request.sortDescriptors = [NSSortDescriptor(key: "category.name", ascending: true)]
+        let fetchedResultController = NSFetchedResultsController(
+            fetchRequest: request,
+            managedObjectContext: context,
+            sectionNameKeyPath: "category.name",
+            cacheName: nil
+        )
+        fetchedResultController.delegate = self
+        try? fetchedResultController.performFetch()
+        return fetchedResultController
+    }()
+    
+    init(dataStore: DataStore, delegate: StoreDelegate?) {
         self.dataStore = dataStore
         self.context = dataStore.managedObjectContext
+        self.delegate = delegate
         super.init()
     }
     
-    override convenience init() {
-        self.init(dataStore: DataStore.shared)
+    convenience init(delegate: StoreDelegate? = nil) {
+        self.init(dataStore: DataStore.shared, delegate: delegate)
+    }
+    
+    private func convert(weekDay: WeekDay) -> WeekDayManagedObject {
+        let weekDayManagedObject = WeekDayManagedObject(context: context)
+        weekDayManagedObject.index = Int16(weekDay.numberValue)
+        return weekDayManagedObject
+    }
+    
+    private func convert(managedObject: WeekDayManagedObject) -> WeekDay? {
+        let index = Int(managedObject.index)
+        switch index {
+        case 1: return WeekDay.sunday
+        case 2: return WeekDay.monday
+        case 3: return WeekDay.tuesday
+        case 4: return WeekDay.wednesday
+        case 5: return WeekDay.thursday
+        case 6: return WeekDay.friday
+        case 7: return WeekDay.saturday
+        default: return nil
+        }
     }
     
     private func convert(tracker: Tracker) -> TrackerManagedObject {
@@ -22,8 +59,39 @@ final class TrackerStore: NSObject {
         trackerManagedObject.name = tracker.name
         trackerManagedObject.emoji = tracker.emoji
         trackerManagedObject.hexColor = ColorMarshall.shared.encode(color: tracker.color)
-        trackerManagedObject.schedule = tracker.schedule.map { $0.rawValue }
+        tracker.schedule.forEach { trackerManagedObject.addToSchedule(convert(weekDay: $0)) }
         return trackerManagedObject
+    }
+    
+    private func convert(managedObject: TrackerManagedObject) -> Tracker? {
+        guard
+            let id = managedObject.id,
+            let name = managedObject.name,
+            let emoji = managedObject.emoji,
+            let hexColor = managedObject.hexColor,
+            let scheduleManagedObjects = managedObject.schedule?.array as? [WeekDayManagedObject]
+        else { return nil }
+        return Tracker(
+            id: id,
+            name: name,
+            color: ColorMarshall.shared.decode(hexColor: hexColor),
+            emoji: emoji,
+            schedule: scheduleManagedObjects.compactMap { convert(managedObject: $0) }
+        )
+    }
+}
+
+extension TrackerStore {
+    var isEmpty: Bool {
+        fetchedResultController.sections?.isEmpty ?? true
+    }
+    
+    var numberOfSections: Int {
+        fetchedResultController.sections?.count ?? 0
+    }
+    
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        fetchedResultController.sections?[section].numberOfObjects ?? 0
     }
     
     func add(_ tracker: Tracker, to category: Category) {
@@ -38,6 +106,61 @@ final class TrackerStore: NSObject {
                 categoryManagedObject.addToTrackers(trackerManagedObject)
                 try context.save()
             }
+        }
+    }
+    
+    func object(at indexPath: IndexPath) -> Tracker? {
+        let trackerManagedObject = fetchedResultController.object(at: indexPath)
+        return convert(managedObject: trackerManagedObject)
+    }
+    
+    func header(at indexPath: IndexPath) -> String? {
+        fetchedResultController.sections?[indexPath.section].name
+    }
+    
+    func filter(by weekDayNumber: Int, and searchText: String? = nil) {
+        let weekDayPredicate = NSPredicate(format: "ANY schedule == %i", weekDayNumber - 1)
+        fetchedResultController.fetchRequest.predicate = weekDayPredicate
+        guard let searchText else {
+            try? fetchedResultController.performFetch()
+            return
+        }
+        let namePredicate = NSPredicate(format: "%K CONTAINS[c] %@", "name", searchText)
+        let compoundPredicate = NSCompoundPredicate(type: .and, subpredicates: [namePredicate, weekDayPredicate])
+        fetchedResultController.fetchRequest.predicate = compoundPredicate
+        try? fetchedResultController.performFetch()
+    }
+}
+
+// MARK: - NSFetchedResultsControllerDelegate
+
+extension TrackerStore: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        insertedIndexes = IndexSet()
+        deletedIndexes = IndexSet()
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        delegate?.didUpdate(
+            StoreUpdate(
+                insertedIndexes: insertedIndexes!,
+                deletedIndexes: deletedIndexes!
+            )
+        )
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .delete:
+            if let indexPath = indexPath {
+                deletedIndexes?.insert(indexPath.item)
+            }
+        case .insert:
+            if let indexPath = newIndexPath {
+                insertedIndexes?.insert(indexPath.item)
+            }
+        default:
+            break
         }
     }
 }
